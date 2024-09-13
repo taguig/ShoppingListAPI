cb9c859ff8bf43836728ddb2b45f9494b3dcacd0
Abdallah <hameurlain.abdallah@gmail.com>
2024-09-13 23:58:23 +0200

    fix : remove useless files & refactor

--- src/main/java/com/example/shoppingapi/repositories/PersonRepository.java
+++ src/main/java/com/example/shoppingapi/repositories/PersonRepository.java
@@ -2,9 +2,18 @@ package com.example.shoppingapi.repositories;
 
 import com.example.shoppingapi.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface PersonRepository extends JpaRepository<Person,Long> {
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    /**
+     * example jpql & native query
+     */
+    @Query(value = "select * from person", nativeQuery = true)
+    public List<Person> findAllPersons();
 
 }
